b702df0b991f9dbce8f6d850d6abd43cbb03803b
ErickMedinaLavielle <58129138+ErickMedinaLavielle@users.noreply.github.com>
2020-01-17 14:52:20 -0600

    add span

--- src/components/Circle/Circle.js
+++ src/components/Circle/Circle.js
@@ -5,7 +5,9 @@ import "./Circle.scss";
 export default function Circle() {
   return (
     <div className="circle">
-      <div className="circle-1" />
+      <div className="circle-1">
+        <span class="circle-1-1">L</span>
+      </div>
       <div className="circle-2" />
       <div className="circle-3" />
     </div>
