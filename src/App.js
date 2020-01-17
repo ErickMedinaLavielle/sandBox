4638a6b454fe79b046723ed28b3adfd45c0dfc7c
ErickMedinaLavielle <58129138+ErickMedinaLavielle@users.noreply.github.com>
2020-01-16 21:19:00 -0600

    first circle
    circle-1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import "./styles.css";
+import Circle from "./components/Circle";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Circle />
     </div>
   );
 }
---
+++ src/components/Circle/Circle.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./Circle.scss";
+
+export default function Circle() {
+  return (
+    <div className="circle">
+      <div className="circle-1" />
+    </div>
+  );
+}
