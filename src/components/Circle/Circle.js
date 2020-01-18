d3cdc8c4b3b7ff8df8781363f6ec2b9bb54c2ff6
ErickMedinaLavielle <58129138+ErickMedinaLavielle@users.noreply.github.com>
2020-01-17 19:52:45 -0600

    14 orbits

--- src/components/Circle/Circle.js
+++ src/components/Circle/Circle.js
@@ -8,8 +8,45 @@ export default function Circle() {
       <div className="circle-1">
         <span class="circle-1-1">L</span>
       </div>
-      <div className="circle-2" />
-      <div className="circle-3" />
+      <div className="circle-2">
+        <span class="circle-1-1">L</span>
+      </div>
+      <div className="circle-3">
+        <span class="circle-1-1">L</span>
+      </div>
+      <div className="circle-4">
+        <span class="circle-1-1">L</span>
+      </div>
+      <div className="circle-5">
+        <span class="circle-1-1">L</span>
+      </div>
+      <div className="circle-6">
+        <span class="circle-1-1">L</span>
+      </div>
+      <div className="circle-7">
+        <span class="circle-1-1">L</span>
+      </div>
+      <div className="circle-8">
+        <span class="circle-1-1">L</span>
+      </div>
+      <div className="circle-9">
+        <span class="circle-1-1">L</span>
+      </div>
+      <div className="circle-10">
+        <span class="circle-1-1">L</span>
+      </div>
+      <div className="circle-11">
+        <span class="circle-1-1">L</span>
+      </div>
+      <div className="circle-12">
+        <span class="circle-1-1">L</span>
+      </div>
+      <div className="circle-13">
+        <span class="circle-1-1">L</span>
+      </div>
+      <div className="circle-14">
+        <span class="circle-1-1">L</span>
+      </div>
     </div>
   );
 }
